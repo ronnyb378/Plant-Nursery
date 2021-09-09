6d303ef6b9799a4b2ebf05eb4dcb2ac647813c6d
Ronny Barahona <ronnybarahona@Ronnys-MacBook-Air.local>
2021-09-09 14:47:20 -0500

    Login form basic template

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
 import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Login() {
     const [username, setUsername] = useState('')
@@ -10,18 +11,22 @@ export default function Login() {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                <span>Username</span>
-                <input vlaue={username} onChange={e => setUsername(e.target.value)}/>
-            </label>
-            <br />
-            <label>
-                <span>Password</span>
-                <input vlaue={password} onChange={e => setPassword(e.target.value)}/>
-            </label>
-            <br />
-            <button type="submit">Login</button>
-        </form>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <span>Username</span>
+                    <input vlaue={username} onChange={e => setUsername(e.target.value)}/>
+                </label>
+                <br />
+                <label>
+                    <span>Password</span>
+                    <input vlaue={password} onChange={e => setPassword(e.target.value)}/>
+                </label>
+                <br />
+                <button type="submit">Login</button>
+            </form>
+                <Link to="/signup">Sign Up</Link>
+                <Link to="/garden">Guest</Link>
+        </div>
     )
 }

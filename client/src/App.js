8f97f3aa5f2cfa6335eb8e2b0cf902770778d8a9
Ronny Barahona <ronnybarahona@Ronnys-MacBook-Air.local>
2021-09-09 15:07:35 -0500

    updating

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Login from './pages/Login';
 
 function App() {

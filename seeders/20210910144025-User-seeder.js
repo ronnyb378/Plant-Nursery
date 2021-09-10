fd91b23707002857e2e4feef0272be818ddf556e
Ronny Barahona <ronnybarahona@Ronnys-MacBook-Air.local>
2021-09-10 09:47:12 -0500

    Added a seeder file for User Db, two demo accounts to work with

---
+++ seeders/20210910144025-User-seeder.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    return queryInterface.bulkInsert('Users', [{
+      username: 'guest',
+      password: 'password',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      username: 'testtest',
+      password: 'test1',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }])
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};

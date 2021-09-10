178d48dc749f85d7e37b3e1567697842dcb8e104
Ronny Barahona <ronnybarahona@Ronnys-MacBook-Air.local>
2021-09-10 13:43:20 -0500

    added redux to detect when a user is logged in and stores the user info for the rest of the app

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,32 +1,55 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { actionLoggedIn } from '../redux/actions/user'
 
 export default function Login() {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
+    const dispatch = useDispatch()
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log('clicked')
+        // console.log('clicked')
+        fetch('/api/v1/users/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username,
+                password
+            })
+        })
+        .then(res=>res.json())
+        .then(data=> {
+            if (data.error) {
+                setError(data.error)
+            } else {
+                dispatch(actionLoggedIn(data.user))
+            }
+        })
     }
 
     return (
         <div>
+            {error && (<div className="error">{error}</div>)}
             <form onSubmit={handleSubmit}>
                 <label>
                     <span>Username</span>
-                    <input vlaue={username} onChange={e => setUsername(e.target.value)}/>
+                    <input value={username} onChange={e => setUsername(e.target.value)} />
                 </label>
                 <br />
                 <label>
                     <span>Password</span>
-                    <input vlaue={password} onChange={e => setPassword(e.target.value)}/>
+                    <input value={password} onChange={e => setPassword(e.target.value)} />
                 </label>
                 <br />
                 <button type="submit">Login</button>
             </form>
-                <Link to="/signup">Sign Up</Link>
-                <Link to="/garden">Guest</Link>
+            <Link to="/signup">Sign Up</Link>
+            <Link to="/garden">Guest</Link>
         </div>
     )
 }
--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,33 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form, Button, Container, Row, Col } from 'react-bootstrap'
 
 export default function SignUp() {
-    return (
-        <div>
-            <Container>
-                <Row xs={1} md={2} className="justify-content-center">
-                    <Col>
-                    <h2>Sign up</h2>
-                        <Form>
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>Username or Email</Form.Label>
-                                <Form.Control type="text"/>
-                                <Form.Text className="text-muted">
-                                    We'll never share your email with anyone else.
-                                </Form.Text>
-                            </Form.Group>
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('')
 
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" />
-                            </Form.Group>
-                            <Button type="submit">
-                                Sign me up!
-                            </Button>
-                        </Form>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    )
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        fetch('/api/v1/users/signup', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username,
+                password
+            })
+        })
+            .then(res => res.json())
+        .then(data => {
+            if (data.error) {
+                setError(data.error)
+            } else {
+                // history.push('/login')
+            }
+        })
+}
+
+return (
+    <div>
+        <Container>
+            <Row xs={1} md={2} className="justify-content-center">
+                <Col>
+                {error && (<div className="error">{error}</div>)}
+                    <h2>Sign up</h2>
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Username or Email</Form.Label>
+                            <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)}/>
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)}/>
+                        </Form.Group>
+                        <Button type="submit">
+                            Sign me up!
+                        </Button>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
+    </div>
+)
 }

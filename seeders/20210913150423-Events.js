69e8f26dd64e42f0e542b27c772d0268944172e7
ayaabedi <alabedi.aya@gmail.com>
2021-09-13 10:11:34 -0500

    created Events seeders file

---
+++ seeders/20210913150423-Events.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+     return queryInterface.bulkInsert("Events", [
+       {
+         plantId: 1,
+         type: 'mixed',
+         notes: 'water 3 times a week',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+
+      }
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};

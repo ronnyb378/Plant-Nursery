1142c97621ace2a465fcff5f687816072cb728b4
Janine Luongo <janineluongo@gmail.com>
2021-09-13 14:27:52 -0400

    added plants POST route

--- routes/plants.js
+++ routes/plants.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models')
 
-// GET all Plants
+// GET all plants
 router.get('/mygarden', async function(req, res, next) {
     console.log("**************SESSION*****************",req.session.user.id)
     await db.Plant.findAll({
@@ -12,14 +12,31 @@ router.get('/mygarden', async function(req, res, next) {
         }
     }).then(plant => {
         if (plant.length < 1) {
-            res.json("No plants b!")
+            res.json("No plants saved for this user!")
         } else {
             res.json(plant)
-        }
-        
-        
+        }       
     })
 })
 
+// POST a new plant
+router.post('/mygarden/:id', async (req, res) => {
+    const user = await db.User.findByPk(req.session.user.id)
+    const plant = await user.createPlant({
+        name: req.body.name,
+        healthrating: req.body.healthrating,
+        species: req.body.species,
+        nickname: req.body.nickname,
+        sun: req.body.sun,
+        waterfrequency: req.body.waterfrequency,
+        activegrowthperiod: req.body.activegrowthperiod,
+        soiltype: req.body.soiltype,
+        fertilizer: req.body.fertilizer,
+        plantdescription: req.body.plantdescription,
+        dateacquired: req.body.dateacquired,
+        location: req.body.location
+    })
+    res.json(plant)
+})
 
 module.exports = router;

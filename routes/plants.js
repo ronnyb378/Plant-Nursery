25c803cbb3fbabbdf798ce21e7669d4bccd1fb81
Janine Luongo <janineluongo@gmail.com>
2021-09-14 11:04:35 -0400

    more plants routes

--- routes/plants.js
+++ routes/plants.js
@@ -20,8 +20,8 @@ router.get('/mygarden', async function(req, res, next) {
 })
 
 // POST a new plant
-router.post('/mygarden/:id', async (req, res) => {
-    const user = await db.User.findByPk(req.session.user.id)
+router.post('/mygarden/newplant', async (req, res) => {
+    // const user = await db.User.findByPk(req.session.user.id)
     const plant = await user.createPlant({
         name: req.body.name,
         healthrating: req.body.healthrating,
@@ -39,9 +39,34 @@ router.post('/mygarden/:id', async (req, res) => {
     res.json(plant)
 })
 
-// // DELETE a plant
-// router.delete('/mygarden', (req, res) => {
+// UPDATE a plant
+router.update('mygarden/:plantId', (req, res) => {
+    
+    db.plant.update({
+        name: req.body.name,
+        healthrating: req.body.healthrating,
+        species: req.body.species,
+        nickname: req.body.nickname,
+        sun: req.body.sun,
+        waterfrequency: req.body.waterfrequency,
+        activegrowthperiod: req.body.activegrowthperiod,
+        soiltype: req.body.soiltype,
+        fertilizer: req.body.fertilizer,
+        plantdescription: req.body.plantdescription,
+        dateacquired: req.body.dateacquired,
+        location: req.body.location
+    })
+    res.json(plant)
+})
 
-// })
+// DELETE a plant
+router.delete('mygarden/:plantId', (req, res) => {
+    const plantId = parseInt(req.params.plantId);
+    db.plants.destroy( {
+        where: { plantId: plantId }
+    })
+    .then(res.json({ message: `Plant with id of ${plantId} successfully deleted.` }),
+    res.redirect('/mygarden'))
+})
 
 module.exports = router;

872886ba4beaca9cc42481a643eccaee4a75dcff
Karen Seunsom <karenseuns@gmail.com>
2021-09-14 11:52:00 -0500

    new plant card created

--- client/src/pages/MyGarden.js
+++ client/src/pages/MyGarden.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react'
-import { Container, Row } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react'
+import { Container, Row} from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
+import AddPlant from '../components/AddPlant'
 import Plant from '../components/Plant'
 import plant1 from '../images/plant1.jpeg'
 import plant2 from '../images/plant2.jpeg'
@@ -8,11 +9,6 @@ import plant3 from '../images/plant3.jpeg'
 import { actionSetResults } from '../redux/actions/results'
 
 export default function MyGarden() {
-
-    // const [show, setShow] = useState(false);
-
-    // const handleClose = () => setShow(false);
-    // const handleShow = () => setShow(true);
 
     const dispatch = useDispatch();
     const results = useSelector((state) => state.results)
@@ -39,33 +35,17 @@ export default function MyGarden() {
                     <img className="plant-pic" src={plant3} alt="plant stems" />
                 </div>
             </div>
-            <Container>
-                <Row sm={2} md={4} lg={6}>
-                    {results.length ? results.map((result) => {
+            {/* PLANTS */}
+            <Container className="text-center">
+                <Row sm={2} md={4} lg={6} className="justify-content-center">
+                    <AddPlant />
+                    {results.length ? (results.map((result) => {
                         return <Plant key={result.id} data={result} />
-                    }) : ('')}
+                    })) : ('')}
+                    
                 </Row>
             </Container>
-            {/* <>
-                <Button variant="primary" onClick={handleShow}>
-                    Launch demo modal
-                </Button>
 
-                <Modal show={show} onHide={handleClose} animation={false}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>Modal heading</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-                    <Modal.Footer>
-                        <Button variant="secondary" onClick={handleClose}>
-                            Close
-                        </Button>
-                        <Button variant="primary" onClick={handleClose}>
-                            Save Changes
-                        </Button>
-                    </Modal.Footer>
-                </Modal>
-            </> */}
         </div>
     )
 }

e97204693e1af86d8a0492246ac15efc31e0d613
Janine Luongo <janineluongo@gmail.com>
2021-09-14 16:55:55 -0400

    built Nav Bar toggle

--- client/src/App.js
+++ client/src/App.js
@@ -28,11 +28,12 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <NavBar />
+        {/* <NavBar /> */}
         <Container className="pt-4 pb-4">
           <Switch>
             <Route exact path="/login">
-              <Login />
+              <NavBar NavBarToggle={false} />
+              <Login  />
             </Route>
             <Route path="/signup">
               <SignUp />
@@ -41,7 +42,7 @@ function App() {
               <Profile />
             </Route>
             <Route path="/mygarden">
-              <MyGarden />
+              <MyGarden NavBarToggle={true} />
             </Route>
             <Route path="/:user/:plantId">
               <Plant />

cee8102a183ebbb74a7856d1430467479009ecbb
Karen Seunsom <karenseuns@gmail.com>
2021-09-17 10:26:43 -0500

    plant page rough draft

--- client/src/pages/Plant.js
+++ client/src/pages/Plant.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import { Dropdown, DropdownButton, FloatingLabel, Form, Button, Container, Row, Col, Image } from 'react-bootstrap'
+import plant from '../images/placeholderPlant.jpeg'
 
 export default function Plant() {
     return (
         <div>
             <h2>plant</h2>
+            {/* <Container> */}
+                <Row style={{height: "300px"}} xs={1} sm={1} md={2} className="justify-content-center align-items-center">
+                    <Col >
+                    <Image src={plant} alt="placeholder plant" fluid/>
+                    <DropdownButton id="dropdown-item-button" title="Dropdown button">
+                        <Dropdown.ItemText>Dropdown item text</Dropdown.ItemText>
+                        <Dropdown.Item as="button">Action</Dropdown.Item>
+                        <Dropdown.Item as="button">Another action</Dropdown.Item>
+                        <Dropdown.Item as="button">Something else</Dropdown.Item>
+                    </DropdownButton>
+                    <FloatingLabel controlId="floatingTextarea2" label="Comments">
+                        <Form.Control
+                            as="textarea"
+                            placeholder="Leave a comment here"
+                            style={{ height: '100px' }}
+                        />
+                    </FloatingLabel>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                    </Col>
+                    <Col className="h-100">
+                        {/* Consider putting calendar in this column */}
+                    </Col>
+                </Row>
+            {/* </Container> */}
         </div>
     )
 }

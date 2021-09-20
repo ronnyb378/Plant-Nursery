15c58e4663e0807155b3495d344e76b56baf6fec
Karen Seunsom <karenseuns@gmail.com>
2021-09-20 13:44:36 -0500

    notes added in storejs

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -9,6 +9,9 @@ const rootReducer = combineReducers({
     status: status
 })
 
+// SESSION NOTES: sessionStorage is cleared when page session ends
+// page session lasts as long as tab or browser is open, and survives over page reloads
+
 const saveToSessionStorage = (globalState) => {
     try {
         const serializedState = JSON.stringify(globalState);
@@ -30,12 +33,15 @@ const loadFromSessionStorage = () => {
 
 const persistedState = loadFromSessionStorage()
 
+// SESSION NOTES: createStore has a second optional argument, '[preloadedState]'
 const store = createStore(
     rootReducer, persistedState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
+// SESSION NOTES: store.subscribe adds a change listener
 store.subscribe(() => {
+    // SESSION NOTES: store.getState() returns current state tree of application. 
     saveToSessionStorage(store.getState())
 })
 

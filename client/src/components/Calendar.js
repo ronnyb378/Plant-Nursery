7d841b47c241d9e61e50af6dd3bc1ff235bddf57
Ronny Barahona <ronnybarahona@Ronnys-MacBook-Air.local>
2021-09-20 15:58:52 -0500

    code is appending new event to calendar. the events are not rendering

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -3,6 +3,7 @@ import FullCalendar from '@fullcalendar/react' // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 
 export default function Calendar({ plantId }) {
+
     const eventsArray = [
         {title: 'event 1', date: '2021-09-15'},
         {title: 'event 2', date: '2021-09-10'}
@@ -14,8 +15,15 @@ export default function Calendar({ plantId }) {
             .then(data=> {
                 // console.log(data)
                 data.forEach(event => {
-                    const createAt = event.createAt
-                    console.log(event, createAt)
+                    const createdAt =(event.createdAt).slice(0,10)
+                    const title = event.type
+                    // console.log(event)
+                    // console.log(createdAt)
+
+                    const oneEvent = {title: `${title}`, date: `${createdAt}`}
+                    // console.log(oneEvent)
+                    eventsArray.push(oneEvent)
+                    console.log(eventsArray)
                 });
 
             })
